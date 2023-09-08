ca75d4bf50882624bd602c07696efb5563db7f94
y.miyake <yuto.miyake@nijibox.co.jp>
2023-09-08 10:34:52 +0900

    画像追加

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { FC, memo } from "react";
+
+import styles from "@/styles/components/header.module.scss";
+
+const Header: FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__content}>
+        <h1 className={styles.header__logoWrap}>
+          <Image
+            className={styles.header__logo}
+            src="images/logo.svg"
+            alt="Furniture Design"
+            loading="lazy"
+            height={20}
+            width={180}
+          />
+        </h1>
+        <button className={styles.header__hamburger}></button>
+      </div>
+    </header>
+  );
+};
+
+export default memo(Header);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,11 @@
-import Image from "next/image";
+import Header from "@/components/Header";
 import { FC } from "react";
-
-import styles from "@/styles/components/header.module.scss";
 
 const HomePage: FC = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles.header__content}>
-        <h1 className={styles.header__logoWrap}>
-          <Image
-            className={styles.header__logo}
-            src="images/logo.svg"
-            alt="Furniture Design"
-            loading="lazy"
-            height={20}
-            width={180}
-          />
-        </h1>
-        <button className={styles.header__hamburger}></button>
-      </div>
-    </header>
+    <>
+      <Header />
+    </>
   );
 };
 
