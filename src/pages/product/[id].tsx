2d160c83164712ac56fd8cba5f2570c5c2c71437
y.miyake <dokyuto106412@gmail.com>
2023-09-11 08:43:52 +0900

    一旦コミット

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,6 +4,9 @@ import { GetStaticProps } from "next";
 import Image from "next/image";
 import { FC } from "react";
 
+import styles from "@/styles/components/productDetail.module.scss";
+import Link from "next/link";
+
 interface Props {
   product: {
     id: string;
@@ -20,27 +23,42 @@ const ProductDetailPage: FC<Props> = ({ product }) => {
   console.log(product);
   return (
     <BaseLayout>
-      <section>
-        <div>
-          <div>
-            <h2>{product.title}</h2>
-            <div>
-              <Image
-                src={product.image}
-                alt={product.title}
-                width={400}
-                height={400}
-              />
-              <div>
-                <p>{product.description}</p>
-                <p>
-                  &yen; {product.price.toLocaleString()}
-                  <small> +tax</small>
-                </p>
-              </div>
+      <section className={styles.productDetail}>
+        <div className={styles.productDetail__content}>
+          <h2 className={styles.productDetail__sectionTitle}>
+            {product.title}
+          </h2>
+          <div className={styles.productDetail__infoWrap}>
+            <Image
+              className={styles.productDetail__image}
+              src={product.image}
+              alt={product.title}
+              width={400}
+              height={400}
+            />
+            <div className={styles.productDetail__info}>
+              <p>{product.description}</p>
+              <p>
+                &yen; {product.price.toLocaleString()}
+                <small> +tax</small>
+              </p>
+              <dl className={styles.productDetail__definitionList}>
+                <div>
+                  <dt>Size:</dt>
+                  <dd>{product.size}</dd>
+                </div>
+                <div>
+                  <dt>Color:</dt>
+                  <dd>{product.color}</dd>
+                </div>
+                <div>
+                  <dt>Material:</dt>
+                  <dd>{product.Material}</dd>
+                </div>
+              </dl>
             </div>
           </div>
-          <a href="/">Back To Products</a>
+          <Link href="/">Back To Products</Link>
         </div>
       </section>
     </BaseLayout>
